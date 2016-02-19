ea9ac89ffc9f5c95be3d159a4045d1c1a3f71c32
cyril tata <cyril.tata@gmail.com>
2016-02-19 11:06:33 +0100

    new document view, some translations

--- src/Products/GEMI/Extensions/Install.py
+++ src/Products/GEMI/Extensions/Install.py
@@ -13,3 +13,25 @@ def uninstall(portal, reinstall=False):
    # @todo remove added properties to objects (IProductsGEMISettings)
     if not reinstall:
         runProfile(portal, 'profile-Products.GEMI:uninstall')
+
+def update_views(brains, original, target):
+    for brain in brains:
+        obj = brain.getObject()
+        if getattr(obj, "layout", None) in original:
+            print "Updated:" + obj.absolute_url()
+            obj.setLayout(target)
+    
+def update_folder_views(portal):
+    portal_catalog = getToolByName(portal, 'portal_catalog')
+    brains = portal_catalog(portal_type="Folder")
+    original = ('navigation-like', 'listbycontenttype')
+    target = 'folder_listing'
+    update_views(brains, original, target)
+
+def update_collection_views(portal):
+    portal_catalog = getToolByName(portal, 'portal_catalog')
+    brains = portal_catalog(portal_type=("Collection", "Topic"))
+    original = ('filter_view')
+    target = 'standard_view'
+    update_views(brains, original, target)
+
--- src/Products/GEMI/setuphandlers.py
+++ src/Products/GEMI/setuphandlers.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
-
+import logging
 
 def post_install(context):
     """Post install script"""
     if context.readDataFile('productsgemi_default.txt') is None:
         return
     # Do something during the installation of this package
+    # define_indexes(context)
+    # update_catelog(context, False)
 
 def define_indexes(context):
+    portal = context.getSite()
+    logger = logging.getLogger('Products.GEMI::define_indexes')
     catalog = getToolByName(portal, 'portal_catalog')
     indexes = catalog.indexes()
+
     # Specify the indexes you want, with ('index_name', 'index_type')
     wanted = (('publication_year', 'FieldIndex'),)
 
@@ -23,3 +28,14 @@ def define_indexes(context):
     if len(indexables) > 0:
         logger.info("Indexing new indexes %s.", ', '.join(indexables))
         catalog.manage_reindexIndex(ids=indexables)
+
+def update_catalog(context, clear=True):
+    portal = context.getSite()
+    logger = logging.getLogger('Products.GEMI::update_catalog')
+    logger.info('Updating catalog (with clear=%s) so items in profiles/default/structure are indexed...' % clear )
+    catalog = portal.portal_catalog
+    err = catalog.refreshCatalog(clear=clear)
+    if not err:
+        logger.info('...done.')
+    else:
+        logger.warn('Could not update catalog.')
