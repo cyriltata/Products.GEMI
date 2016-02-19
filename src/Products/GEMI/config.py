d50ad30d803833651714af0f456cdbe1db631476
Cyril Tata <cyril.tata@gmail.com>
2016-02-19 12:14:58 +0100

    translations

--- src/Products/GEMI/config.py
+++ src/Products/GEMI/config.py
@@ -4,16 +4,3 @@ PROJECTNAME = "Products.GEMI"
 
 ALLOWED_TYPES_COLLECTION_FILTER_VIEW = ('ArticleReference',)
 
-ALLOWED_TYPES_CONTENT_TYPE_VIEW = [
-    ('Folder',      _(u'Folder')),
-    ('Document',    _(u'Page')),
-    ('Link',        _(u'Link')),
-    ('Image',       _(u'Image')),
-    ('Topic',       _(u'Collection')),
-    ('Collection',  _(u'Collection (old style)')),
-    ('Event',       _(u'Event')),
-    ('File',        _(u'File')),
-    ('News Item',   _(u'News Item')),
-    ('Form Folder', _(u'Form Folder')),
-    ('Bibliography Folder', _(u'Bibliography Folder'))
-]
