f2bfc0a5c2b3e331c1864ec68051c0b91c36d05a
Cyril <cyril.tata@gmail.com>
2017-03-24 09:45:10 +0100

    add period after identifiers

--- src/Products/GEMI/browser/bibfolder.py
+++ src/Products/GEMI/browser/bibfolder.py
@@ -304,7 +304,7 @@ class ViewListFormatter(BrowserView):
     def getFormattedIdentifiers(self):
         if (not hasattr(self.item, 'identifiers')):
             return None
-        return ' '.join([" %s:%s," % (identifier['label'], identifier['value']) for identifier in self.item.getIdentifiers()]).strip(',');
+        return ' '.join([" %s:%s," % (identifier['label'], identifier['value']) for identifier in self.item.getIdentifiers()]).strip(',') + '.';
 
     def getPages(self):
         if (hasattr(self.item, 'pages') and self.item.getPages() and not self.inBook()):
